cd2816163fea37375f779faf24b73634d9902035
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-01-11 12:51:03 -0300

    feat(footer): Inícia a criação do componente footer
    Começa a criar o design no CSS e organização do footer no HTML.

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,9 +1,42 @@
+import styles from "./footer.module.css";
+
+import logo from "../../assets/images/logo.png";
+import instagramIcon from "../../assets/images/icons/Instagram.png";
+import gmailIcon from "../../assets/images/icons/Gmail.png";
+
 function Footer() {
-    return (
-        <footer>
-            <h3>Footer</h3>
-        </ footer>
-    )
+  return (
+    <footer className={styles.footerContainer}>
+      <img src={logo} className={styles.footerLogo} />
+
+      <section>
+        <article>
+          <h4>Páginas:</h4>
+          <a href="">Início</a>
+          <a href="">Sobre</a>
+          <a href="">Contato</a>
+          <a href="">Patrocinadores</a>
+
+          <h4 className="italic">Nos Contate:</h4>
+          <a href="">
+            <img src={instagramIcon} alt="Instagram" className={styles.icon} />
+          </a>
+          <a href="">
+            <img src={gmailIcon} alt="Gmail" className={styles.icon} />
+          </a>
+        </article>
+
+        <article>
+          <form>
+            <label htmlFor="nome"></label>
+            <input type="text" name="nome">
+              Seu nome
+            </input>
+          </form>
+        </article>
+      </section>
+    </footer>
+  );
 }
 
 export default Footer;
