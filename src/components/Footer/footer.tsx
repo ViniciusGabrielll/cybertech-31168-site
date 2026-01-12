dd651d7c6bea4dc659f74a7b965a099f65daf6a6
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-01-12 18:44:51 -0300

    feat(footer): Finaliza estilização do Footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -4,21 +4,20 @@ import logo from "../../assets/images/logo.png";
 import instagramIcon from "../../assets/images/icons/Instagram.png";
 import gmailIcon from "../../assets/images/icons/Gmail.png";
 import Form from "../Form/form";
-import patrocinadores from "../../assets/images/patrocinadoresFooter.png"
+import patrocinadores from "../../assets/images/patrocinadoresFooter.png";
 
 function Footer() {
-
   return (
     <footer className={styles.footerContainer}>
       <img src={logo} className={styles.footerLogo} />
 
       <section>
-        <article>
+        <article className={styles.paginasFooter}>
           <h4>Páginas:</h4>
-          <a href="">Início</a>
-          <a href="">Sobre</a>
-          <a href="">Contato</a>
-          <a href="">Patrocinadores</a>
+          <a href="" className={styles.link}>Início</a>
+          <a href="" className={styles.link}>Sobre</a>
+          <a href="" className={styles.link}>Contato</a>
+          <a href="" className={styles.link}>Patrocinadores</a>
 
           <h4 className="italic">Nos Contate:</h4>
           <a href="">
@@ -31,13 +30,30 @@ function Footer() {
 
         <article className={styles.contatoFooter}>
           <h4>Manda um email!</h4>
-          <Form/>
+          <Form />
         </article>
 
         <article>
-          <img src={patrocinadores} className={styles.footerPatrocinadores}></img>
+          <img
+            src={patrocinadores}
+            className={styles.footerPatrocinadores}
+          ></img>
         </article>
       </section>
+
+      <small>
+        <a href="https://viniciusgabriel.netlify.app/" className={styles.link}>
+          Copyright (c) 2026 Vinícius Gabriel
+        </a>
+      </small>
+      <small>
+        <a
+          href="https://github.com/ViniciusGabrielll/cybertech-31168-site?tab=readme-ov-file"
+          className={styles.link}
+        >
+          Site no github
+        </a>
+      </small>
     </footer>
   );
 }
