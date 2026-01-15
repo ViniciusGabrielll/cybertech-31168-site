f9671b74c1637205998f26ac424a2601c3f6bdf6
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-01-14 21:43:09 -0300

    style(Responsividade): Cria menu hamburguer
    - Cria menu hamburguer mas ainda não pronto.
    
    - Corrige footer.module.css e form.module.css para maior responsividade

--- src/components/NavBar/navBar.tsx
+++ src/components/NavBar/navBar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import "../../main.css";
 import styles from "./navBar.module.css";
 import Dropdown from "../DropdownButton/dropDown";
 import logo from "../../assets/images/logo.png";
@@ -7,41 +8,58 @@ import gmailIcon from "../../assets/images/icons/Gmail.png";
 
 function NavBar() {
   const [hidden, setHidden] = useState(false);
+  const [linksHamburguer, setLinksHamburguer] = useState(false);
   const lastScrollY = useRef(0);
 
-  useEffect (() => {
+  useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
-      if(currentScrollY > lastScrollY.current) {
+      if (currentScrollY > lastScrollY.current) {
         setHidden(true);
       } else {
         setHidden(false);
       }
 
       lastScrollY.current = currentScrollY;
-    }
+    };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   });
 
+  function toggle() {
+    setLinksHamburguer((prev) => !prev);
+  }
+
   return (
     <nav className={`${styles.container} ${hidden ? styles.hidden : ""}`}>
       <div>
-        <img src={logo} alt="CyberTech Logo" className={styles.logo}/>
-        <a href="">Início</a>
-        <a href="">Sobre</a>
-        <a href="">Contato</a>
-        <a href="">Patrocinadores</a>
-        <Dropdown />
+        <div>
+          <button className={styles.menuToggle} onClick={toggle}>
+            ☰
+          </button>
+
+          <div
+            className={`${styles.navLinks} ${
+              linksHamburguer ? styles.active : ""
+            }`}
+          >
+            <a href="">Início</a>
+            <a href="">Sobre</a>
+            <a href="">Contato</a>
+            <a href="">Patrocinadores</a>
+            <Dropdown />
+          </div>
+        </div>
+        <img src={logo} alt="CyberTech Logo" className={styles.logo} />
       </div>
       <div>
         <a href="">
-          <img src={instagramIcon} alt="Instagram" className={styles.icon}/>
+          <img src={instagramIcon} alt="Instagram" className={styles.icon} />
         </a>
         <a href="">
-          <img src={gmailIcon} alt="Gmail" className={styles.icon}/>
+          <img src={gmailIcon} alt="Gmail" className={styles.icon} />
         </a>
       </div>
     </nav>
