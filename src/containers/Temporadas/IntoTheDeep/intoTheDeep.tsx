dd0d798bc6d3584dd26e7f212965f08631f79307
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-01-19 19:15:30 -0300

    feat(IntoTheDeep): Aumenta Quantidade de Conteúdo

--- src/containers/Temporadas/IntoTheDeep/intoTheDeep.tsx
+++ src/containers/Temporadas/IntoTheDeep/intoTheDeep.tsx
@@ -3,6 +3,8 @@ import fundoIntoTheDeep from "../../../assets/images/fundoIntoTheDeep.png";
 import fundacao from "../../../assets/images/Fundacao.png";
 import kit from "../../../assets/images/kit.png";
 import mentoria from "../../../assets/images/mentoria.png";
+import robo from "../../../assets/images/roboIntoTheDeep.png";
+import programacao from "../../../assets/images/programacaoIntoTheDeep.png";
 
 function IntoTheDeep() {
   return (
@@ -59,16 +61,82 @@ function IntoTheDeep() {
         <section>
           <h2>Mentoria RevAtom</h2>
           <article>
-            <h3>Mentoria com a <span className="italic">RevAtom</span></h3>
+            <h3>
+              Mentoria com a <span className="italic">RevAtom</span>
+            </h3>
             <p>
               <ul>
-                <li>Objetivo da mentoria – auxiliar no desenvolvimento e adaptação ao FTC.</li>
-                <li>Importância da mentoria – aprendizado acelerado e fortalecimento da equipe iniciante.</li>
-                <li>Impacto na evolução – base sólida para as próximas etapas da temporada.</li>
+                <li>
+                  Objetivo da mentoria – auxiliar no desenvolvimento e adaptação
+                  ao FTC.
+                </li>
+                <li>
+                  Importância da mentoria – aprendizado acelerado e
+                  fortalecimento da equipe iniciante.
+                </li>
+                <li>
+                  Impacto na evolução – base sólida para as próximas etapas da
+                  temporada.
+                </li>
               </ul>
             </p>
           </article>
           <img src={mentoria} alt="Mentoria Com a RevAtom" />
+        </section>
+        <section>
+          <h2>Mecânica do Robô</h2>
+          <img src={robo} alt="Robo da Into The Deep" />
+          <article>
+            <h3>Mecânica do Robô</h3>
+            <p className="negrito">
+              Tech-Hurdle e Mecanismo das Rodas DUO Flap:
+            </p>
+            <p>
+              Durante a criação do protótipo, foi desenvolvido o Tech-Hurdle em
+              conjunto com o mecanismo das Rodas Flap DUO para aprimorar o
+              sistema de intake. As Rodas Flap DUO oferecem maior tolerância e
+              adaptação a diferentes variações de samples, garantindo contato
+              constante durante a coleta. Após testes identificarem a queda de
+              peças, o Tech-Hurdle foi implementado como um compartimento de
+              segurança, mantendo os samples firmes durante a movimentação do
+              robô. Essa integração resultou em maior estabilidade, eficiência e
+              confiabilidade no processo de coleta.
+            </p>
+          </article>
+        </section>
+        <section>
+          <h2>Programação do Robô</h2>
+          <article>
+            <h3>Programação do Robô</h3>
+            <p>
+              A programação do robô foi desenvolvida em Java, com foco em
+              desempenho, precisão e adaptabilidade. Implementamos rotas
+              planejadas para o modo autônomo, garantindo movimentos confiáveis
+              e execução estratégica das tarefas. No modo teleoperado, o sistema
+              oferece controles personalizáveis, permitindo maior domínio do
+              robô durante as partidas. Além disso, a telemetria em tempo real
+              possibilita ajustes rápidos e tomada de decisões mais eficientes,
+              contribuindo para um melhor desempenho em competição.
+            </p>
+          </article>
+          <img src={programacao} alt="Como funciona a Programação do robô" />
+        </section>
+        <section className="ultimaSection">
+          <h2>Impacto</h2>
+          <article>
+            <h3>Impacto</h3>
+            <p className="negrito">Projeto CyberStart:</p>
+            <p>
+              Promovemos a robótica de competição em escolas públicas de Moreno,
+              com a entrega de maletas de robótica à Secretaria de Educação e
+              ações voltadas ao incentivo de jovens no universo da tecnologia.
+              Nosso compromisso é expandir esse impacto no futuro, levando mais
+              kits, técnicos e mentores para fortalecer a prática da robótica.
+              Paralelamente, buscamos o fortalecimento de patrocínios, a
+              expansão do programa FIRST e o desenvolvimento de parcerias com
+              empresas.
+            </p>
+          </article>
         </section>
       </main>
     </>
